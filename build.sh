2965eb7a2124e1b7b4a702f13b0f5e6c3603c573
unknown <ajitkumarseelam1310@gmail.com>
2025-05-28 12:06:52 +0100

    another fix

--- build.sh
+++ build.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 set -e
 
-# Go to project root
-ROOT_DIR=$(pwd)
-
 echo "📦 Installing frontend dependencies"
-cd "$ROOT_DIR/ui"
+cd ui
 npm install
 
 echo "🌐 Exporting Expo web build"
 npx expo export:web
 
 echo "📦 Installing backend dependencies"
-cd "$ROOT_DIR/server"
+cd ../server
 npm install
